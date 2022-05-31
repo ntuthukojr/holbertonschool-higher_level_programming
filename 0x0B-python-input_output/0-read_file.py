43492458bedc22b9667c408c109a96ba9071e049
Ntuthuko Jr Mashinini <ntuthuko@ossconsult.com>
2022-05-31 23:19:11 +0200

    Push Files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-""" function that reads a text file (UTF8) and prints it to stdout """
+"""read_file
+"""
 
 
 def read_file(filename=""):
-    """ Function that reads from a file
-
-    Args:
-        filename: filename
-
-    Raises
-        Exception: when the file can be opened
-
+    """Takes in str filename to read it
     """
 
-    with open(filename, 'r', encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data, end='')
+    with open(filename, encoding="utf-8") as readFile:
+        print(readFile.read(), end='')
