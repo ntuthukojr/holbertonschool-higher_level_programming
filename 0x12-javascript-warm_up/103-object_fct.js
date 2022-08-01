92623bb47d256bd100e9f20029d8c41034754811
Ntuthuko Jr Mashinini <ntuthuko@ossconsult.com>
2022-08-01 14:57:57 +0200

    push file

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
+// adding a new function incr that increments the integer value
 const myObject = {
     type: 'object',
     value: 12
 };
 console.log(myObject);
+
 myObject.incr = function () {
     myObject.value += 1;
 };
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
