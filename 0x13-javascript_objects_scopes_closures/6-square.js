63441c5a147fa384d2112b74c700f656ab86d518
Ntuthuko Jr Mashinini <ntuthuko@ossconsult.com>
2022-08-02 15:45:47 +0200

    push

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-// class Square
-const Sq = require('./5-square');
-module.exports = class Square extends Sq {
+const Rectangle = require('./5-square');
+module.exports = class Square extends Rectangle {
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let j = 0; j < this.height; j++) {
-        console.log(Array(this.height + 1).join(c));
-      }
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 };
