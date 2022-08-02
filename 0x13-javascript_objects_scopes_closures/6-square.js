4cdc2707ce65684bd4b8f0b6b1faefa44fc420d7
Ntuthuko Jr Mashinini <ntuthuko@ossconsult.com>
2022-08-02 15:32:30 +0200

    push

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,25 +1,7 @@
 #!/usr/bin/node
-/*  Class Square that defines a square and inherits from Rectangle */
-const _square = require('./5-square');
-class Square extends _square {
-  constructor (size) {
-    super(size, size);
-  }
+const Square = require('./6-square');
 
-  // Method that prints the rectangle using the character "C" or "X"
-  charPrint (c) {
-    if (c) {
-      let line;
-      for (let i = 0; i < this.height; i++) {
-        line = c;
-        for (let j = 0; j < this.width - 1; j++) {
-          line += c;
-        }
-        console.log(line);
-      }
-    } else {
-      this.print();
-    }
-  }
-}
-module.exports = Square;
+const s1 = new Square(4);
+s1.charPrint();
+
+s1.charPrint('C');
