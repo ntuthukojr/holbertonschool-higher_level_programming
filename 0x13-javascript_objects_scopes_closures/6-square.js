774811599e9672e95c9bccc1571483cd6eea68a1
Ntuthuko Jr Mashinini <ntuthuko@ossconsult.com>
2022-08-02 15:33:35 +0200

    push

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,16 @@
 #!/usr/bin/node
-const Square = require('./6-square');
-
-const s1 = new Square(4);
-s1.charPrint();
-
-s1.charPrint('C');
+const prevSquare = require('./5-square');
+class Square extends prevSquare {
+  // Method to print Square instance using given character or 'X'
+  charPrint (c = 'X') {
+    let row = '';
+    let i = 0;
+    for (i = 0; i < this.width; i++) {
+      row += c;
+    }
+    for (i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;
